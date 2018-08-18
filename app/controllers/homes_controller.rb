1fe539aeb432b96acdf857842cc5907dc9b66999
aloha-inc <naoharuyoshikawa@gmail.com>
2018-08-18 15:43:27 +0900

    about、rankingsコントローラーの作成

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,10 +1,4 @@
 class HomesController < ApplicationController
   def top
   end
-
-  def about
-  end
-
-  def ranking
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'rankings/index'
+  get 'about/index'
 	get "/" => "homes#top"
-  get "/about" => "homes#about"
-  get "/ranking" => "homes#ranking"
+  # get "/about" => "homes#about"
+  # get "/ranking" => "homes#ranking"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
