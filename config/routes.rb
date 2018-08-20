38539636e14cd56d12b3a3d4a66f39b7ad63cde8
aloha-inc <naoharuyoshikawa@gmail.com>
2018-08-20 19:36:46 +0900

    aboutページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'rankings/index'
   get 'about/index'
 	get "/" => "homes#top"
-  # get "/about" => "homes#about"
-  # get "/ranking" => "homes#ranking"
+  get "/about" => "about#index"
+  get "/rankings" => "rankings#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
