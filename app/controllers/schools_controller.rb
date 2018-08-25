89e9806eb1d0d7323dfab5df0d2266f7c3306920
aloha-inc <naoharuyoshikawa@gmail.com>
2018-08-25 13:43:54 +0900

    絞り込み検索機能（purposes,courses）の実装まで

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -5,15 +5,24 @@ class SchoolsController < ApplicationController
   # GET /schools.json
   def index
     @schools = School.all
-    @courses_list = @schools.tag_counts_on(:courses).map(&:name)
+    @courses_list = @schools.tag_counts_on(:courses).map(&:name).unshift("ALL")
+    @purposes_list = @schools.tag_counts_on(:purposes).map(&:name).unshift("ALL")
 
     if params[:name].present? 
       @schools = @schools.get_by_name params[:name]
     end
 
-    if params[:course_list].present?
-    @schools = @schools.get_by_course_list params[:course_list]
-    end
+   # if params[:course_list].present? 
+      @course_tag = params[:course_list]
+      @purpose_tag = params[:purpose_list]
+      @schools =  School.all
+      if @course_tag.present? and @course_tag != "ALL" then
+        @schools =  @schools.tagged_with(@course_tag, :on => :courses)
+      end
+      if @purpose_tag.present? and @purpose_tag != "ALL" then
+        @schools =  @schools.tagged_with(@purpose_tag, :on => :purposes)
+      end
+    # end
   end
 
   # GET /schools/1
